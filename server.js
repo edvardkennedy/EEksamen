538b84b29b146d2222508b192ef5f19f602e16c7
edvardkennedy <edvardspillum07@gmail.com>
2026-05-28 10:57:45 +0200

    Price Fix

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const PORT = 3000;
 let totaltSolgt = 0;
 
 const server = http.createServer((req, res) => {
-    // 1. Hvis noen ber om nettsiden, send HTML-filen
+    // sender til nettside index
     if (req.method === 'GET' && req.url === '/') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
     } 
-    // 2. Hvis noen trykker på "Kjøp", Print terminal
+    // 2. Hvis noen trykker på "Kjøp", Print terminal.
     else if (req.method === 'POST' && req.url === '/nytt-kjop') {
         let body = '';
         req.on('data', chunk => { body += chunk.toString(); });
